55b77f460190a7b2fe5eb269963ee940f35d1da1
Jahfer Husain <jahferh@gmail.com>
2013-10-07 23:20:34 -0400

    Added keyboard navigation to search

--- js/docs.js
+++ js/docs.js
@@ -18,19 +18,60 @@ var API_SEARCH = (function() {
 
   var index = [];
   var query = "";
+  var selectedResult = 0;
 
-  var onKeyUp = function (e) {
-    var query = $(this).val();
+  var nonSearchKeycodes = [40, 38, 13];
+
+  // keyboard navigation
+  var resultSelector = function (e) {
+    if (nonSearchKeycodes.indexOf(e.keyCode) < 0) return;
+
+    var $selected = $('#api-search-results .selected');
+
+    if (e.keyCode === 13) {
+
+      window.location = $selected.children('a').eq(0).attr('href');
+      $('.api-search-input').val('');
+      resetSearch();
+
+    } else {
+
+      var $next = (e.keyCode === 40) ? $selected.next() : $selected.prev();
+
+      if ($next.length) {
+        $selected.removeClass('selected');
+        $next.addClass('selected');
+      }
+
+    }
+
+    return false;
+  };
+
+  var resetSearch = function () {
     $('#api-search-results').empty();
+    selectedResult = 0;
+  };
 
+  var autocomplete = function (e) {
+    if (nonSearchKeycodes.indexOf(e.keyCode) >= 0) return;
+
+    query = $(this).val();
+    resetSearch();
     if (!query) return;
 
     var results = index.filter( filterResults ).slice(0, 7);
 
     results.forEach(function(result) {
-      var content = $('<a>').attr('href', result.link).html(result.name);
+      var content = $('<a>')
+        .addClass('result-link')
+        .attr('href', result.link)
+        .html(result.name);
+
       $('<li>').html(content).appendTo('#api-search-results');
     });
+
+    $('#api-search-results').children().eq(0).addClass('selected');
   };
 
   var filterResults = function (item) {
@@ -49,7 +90,8 @@ var API_SEARCH = (function() {
 
   var init = function (searchIndex) {
     index = searchIndex;
-    $('.api-search-input').on('keyup', onKeyUp);
+    $('.api-search-input').on('keyup', autocomplete);
+    $('.api-search-input').on('keydown', resultSelector);
   };
 
   return { init: init };
